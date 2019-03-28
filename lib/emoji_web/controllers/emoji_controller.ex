9619dcc1db8405acc57378accedcb5b128e69a6f
Travis Bogosian <travis.m.bogosian@gmail.com>
2019-03-28 15:22:58 -0400

    minor indentation cleanup

--- lib/emoji_web/controllers/emoji_controller.ex
+++ lib/emoji_web/controllers/emoji_controller.ex
@@ -5,30 +5,27 @@ defmodule EmojiWeb.EmojiController do
   def lookupKey, do: @lookupKey
 
   def show(conn, %{@lookupKey => "sparkles"}), do: processEmoji("✨", conn)
-
   def show(conn, %{@lookupKey => "vulcan"}), do: processEmoji("🖖", conn)
-
   def show(conn, %{@lookupKey => "white_check_mark"}), do: processEmoji("✅", conn)
   def show(conn, %{@lookupKey => "white check mark"}), do: processEmoji("✅", conn)
-
   def show(conn, %{@lookupKey => "nail_care"}), do: processEmoji("💅", conn)
   def show(conn, %{@lookupKey => "nail care"}), do: processEmoji("💅", conn)
 
   def show(conn, %{@lookupKey => emoji}) do
     emoji
-      |> Exmoji.find_by_short_name
-      |> Enum.filter(&(&1.short_name == emoji))
-      |> Enum.map(&(Exmoji.EmojiChar.render/1))
-      |> showEmojiLookupResult(conn)
+    |> Exmoji.find_by_short_name()
+    |> Enum.filter(&(&1.short_name == emoji))
+    |> Enum.map(&(Exmoji.EmojiChar.render/1))
+    |> showEmojiLookupResult(conn)
   end
 
   def showEmojiLookupResult([emoji|_], conn), do: processEmoji(emoji, conn)
 
   def showEmojiLookupResult(_, conn) do
     conn
-      |> put_status(:not_found)
-      |> put_view(EmojiWeb.ErrorView)
-      |> render("404.html")
+    |> put_status(:not_found)
+    |> put_view(EmojiWeb.ErrorView)
+    |> render("404.html")
   end
 
   defp processEmoji(emoji, conn) do
